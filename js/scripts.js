5206222cc89673b9a7b8aa965955cb3a0116f766
x00117003 <iramofu@yahoo.com>
2020-12-17 18:40:59 +0000

    Add files via upload
    added css, js and images

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+(function($) {
+    "use strict"; 
+	
+	/* Navbar Scripts */
+	// jQuery to collapse navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 20) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+
+    // close menu on click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    
+})(jQuery);
